52e339c0c31d169ade48e2ccfdc925ebcd63cb8c
Dave Boggs <dave.boggs@citrix.com>
2020-09-29 00:33:58 -0400

    Routes and clue pages set up.

---
+++ routes.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, make_response
+
+app = Flask(__name__,template_folder='templates')
+
+
+@app.route('/')
+def hello_world():
+    return render_template('question6.html')
+
+@app.route('/4531361')
+def puzzle2():
+
+    return 'Hello, World!'
+
+@app.route('/4776762')
+def puzzle3():
+    answer = "anything"
+    return 'Hello, World!'
+
+@app.route('/7256119')
+def puzzle4():
+    answer = "11"
+    return 'Hello, World!'
+
+@app.route('/6131894')
+def puzzle5():
+    answer = "Ben Afquack"
+    return 'Hello, World!'
+
+@app.route('/3611839')
+def puzzle6():
+    answer = "1999"
+    return 'Hello, World!'
---
+++ runServer.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=routes.py
+set FLASK_ENV=development
+python -m flask run
