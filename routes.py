3c220d22902ba2e35994974804794a219c13f844
Dave Boggs <dave.boggs@citrix.com>
2020-09-30 20:02:46 -0400

    f

--- routes.py
+++ routes.py
@@ -1,36 +1,41 @@
 from flask import Flask, render_template, make_response
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def hello_world():
-    return render_template('question6.html')
+	return render_template('question6.html')
+
 
 @app.route('/4531361')
 def puzzle2():
+	return 'Hello, World!'
 
-    return 'Hello, World!'
 
 @app.route('/4776762')
 def puzzle3():
-    answer = "anything"
-    return 'Hello, World!'
+	answer = "anything"
+	return 'Hello, World!'
+
 
 @app.route('/7256119')
 def puzzle4():
-    answer = "11"
-    return 'Hello, World!'
+	answer = "11"
+	return 'Hello, World!'
+
 
 @app.route('/6131894')
 def puzzle5():
-    answer = "Ben Afquack"
-    return 'Hello, World!'
+	answer = "Ben Afquack"
+	return 'Hello, World!'
+
 
 @app.route('/3611839')
 def puzzle6():
-    answer = "1999"
-    return 'Hello, World!'
+	answer = "1999"
+	return 'Hello, World!'
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', port=environ.get("PORT", 80))
